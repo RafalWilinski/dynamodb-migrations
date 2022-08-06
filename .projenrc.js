b770e050329066e14d897ba9d2c2eafd4c5ca7a6
Rafal Wilinski <raf.wilinski@gmail.com>
2022-08-06 17:56:56 +0200

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,12 @@
+const { FunctionlessProject } = require('@functionless/projen');
+const project = new FunctionlessProject({
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  devDeps: ['@functionless/projen'],
+  name: 'dynamodb-mass-update',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
