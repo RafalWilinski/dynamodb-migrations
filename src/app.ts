df5e7852784cb6390ceef18abf27dbed98cc7e80
Rafal Wilinski <raf.wilinski@gmail.com>
2022-08-07 21:26:42 +0200

    chore: add todos

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { App, Stack } from "aws-cdk-lib";
+import { aws_dynamodb, Stack } from "aws-cdk-lib";
 import { Table as cdkTable } from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 import {
@@ -11,9 +11,6 @@ import {
 } from "functionless";
 import { ScanOutput } from "typesafe-dynamodb/lib/scan";
 
-const app = new App();
-const stack = new Stack(app, "stack");
-
 export type ScanTableOptions = {
   segments: number;
 };
@@ -22,18 +19,48 @@ export type MigrationProps = {
   tableArn: string;
 };
 
+export type MigrationManagerProps = {
+  tableName: string;
+};
+
+export type MigrationHistoryItem = {
+  id: string;
+  status: "success" | "in_progress" | "failure";
+  startedAt: string;
+  endedAt: string;
+  segments: number;
+  completedSegments?: number[];
+};
+
 export type TransformFunctionType<T extends object> = (
   _table: ITable<T, any, any>,
   result: ScanOutput<any, any, any>
 ) => Promise<any>;
 
-export class Migration<T extends object> extends Construct {
+export class MigrationsManager extends Construct {
+  public readonly migrationsHistoryTable: Table<MigrationHistoryItem, "id">;
+
+  constructor(scope: Construct, id: string, props: MigrationManagerProps) {
+    super(scope, id);
+
+    this.migrationsHistoryTable = new Table(scope, "MigrationsHistoryTable", {
+      tableName: props.tableName,
+      partitionKey: {
+        name: "id",
+        type: aws_dynamodb.AttributeType.STRING,
+      },
+    });
+  }
+}
+
+export class Migration<T extends object> extends Stack {
   public readonly table: ITable<T, any, any>;
+
   constructor(scope: Construct, id: string, props: MigrationProps) {
     super(scope, id);
 
     this.table = Table.fromTable(
-      cdkTable.fromTableArn(stack, "SubjectTable", props.tableArn)
+      cdkTable.fromTableArn(scope, "SubjectTable", props.tableArn)
     );
   }
 
@@ -44,7 +71,10 @@ export class Migration<T extends object> extends Construct {
     const totalSegments = options?.segments ?? 10;
     const segments = Array.from({ length: totalSegments }, (_, i) => i);
 
-    new StepFunction(stack, "MigrationStepFunction", async () => {
+    // todo: fetch migrations history table name
+    // todo: add migration entry "in_progress"
+
+    new StepFunction(this, "MigrationStepFunction", async () => {
       return $SFN.map(segments, async (_, index) => {
         let lastEvaluatedKey;
         let firstRun = true;
@@ -61,11 +91,13 @@ export class Migration<T extends object> extends Construct {
           result.LastEvaluatedKey = result.LastEvaluatedKey;
 
           new Function(
-            stack,
+            this,
             "MigrationCallbackFunction",
             await transformFn(this.table, result)
           );
         }
+
+        // todo: add migration entry "completed" for some segment?
       });
     });
 
--- src/migrations/20220801-add-attribute.ts
+++ src/migrations/20220801-add-attribute.ts
@@ -10,23 +10,20 @@ export type MigrationFunction = (
 ) => Migration<any>;
 
 export const up: MigrationFunction = (scope, migrationName) =>
-  // Initialize Migration Object
+  // Initialize Migration Stack
   new Migration(scope, migrationName, {
     tableArn: "arn:aws:dynamodb:us-east-1:123456789012:table/SubjectTable",
-  }).run(
-    async (_table, result: ScanOutput<any, any, any>) => {
-      for (const i of result.Items as any[]) {
-        await $AWS.DynamoDB.PutItem({
-          Table: _table,
-          Item: {
-            id: {
-              S: `${i}_migrated`,
-            },
+  }).run(async (_table, result: ScanOutput<any, any, any>) => {
+    // Actual migration code goes here.
+    // Do something with each item in the table.
+    for (const i of result.Items as any[]) {
+      await $AWS.DynamoDB.PutItem({
+        Table: _table,
+        Item: {
+          id: {
+            S: `${i}_migrated`,
           },
-        });
-      }
-    },
-    {
-      segments: 5,
+        },
+      });
     }
-  );
+  });
