ff7bfa8031de043c597f6dab69cdf639465c3b4c
Rafal Wilinski <raf.wilinski@gmail.com>
2022-08-10 15:00:05 +0200

    feat: release

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,11 @@ const { FunctionlessProject } = require("@functionless/projen");
 const project = new FunctionlessProject({
   cdkVersion: "2.1.0",
   defaultReleaseBranch: "main",
-  devDeps: ["@functionless/projen"],
+  devDeps: [
+    "@functionless/projen",
+    "@types/change-case",
+    "@types/lodash.sortby",
+  ],
   name: "dynamodb-mass-update",
   eslintOptions: {
     prettier: true,
@@ -12,6 +16,16 @@ const project = new FunctionlessProject({
     "deploy-example":
       'cdk deploy --app "npx ts-node src/examples/test-stack.ts"',
   },
+  description:
+    "Functionless-based mini-framework for DynamoDB migrations in AWS CDK.",
+  deps: [
+    "@aws-sdk/client-sfn",
+    "@types/aws-lambda",
+    "change-case",
+    "lodash.sortby",
+  ],
+  release: true,
+  packageName: "@dynobase/dynamodb-migrations",
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
