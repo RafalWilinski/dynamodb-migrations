029289204f449b4d952f35b2d860fba7a839c0ae
Rafal Wilinski <raf.wilinski@gmail.com>
2022-08-10 13:02:52 +0200

    fix: return status in awaiter

--- src/custom-resource-is-migration-complete.ts
+++ src/custom-resource-is-migration-complete.ts
@@ -3,10 +3,7 @@ import {
   ExecutionStatus,
   SFNClient,
 } from "@aws-sdk/client-sfn";
-import {
-  CloudFormationCustomResourceEvent,
-  CloudFormationCustomResourceResponse,
-} from "aws-lambda";
+import { CloudFormationCustomResourceEvent } from "aws-lambda";
 import { Construct } from "constructs";
 import { $AWS, Function, Table } from "functionless";
 import { marshall } from "typesafe-dynamodb/lib/marshall";
@@ -22,10 +19,12 @@ export type CustomResourceIsMigrationCompleteCheckerProps = {
   migrationsHistoryTable: Table<MigrationHistoryItem, "id">;
 };
 
+export type ReturnType = { IsComplete: boolean };
+
 export default class CustomResourceIsMigrationCompleteChecker extends Construct {
   public readonly function: Function<
     CloudFormationCustomResourceEvent,
-    CloudFormationCustomResourceResponse
+    ReturnType
   >;
   constructor(
     scope: Construct,
@@ -37,67 +36,52 @@ export default class CustomResourceIsMigrationCompleteChecker extends Construct 
   ) {
     super(scope, id);
 
+    let allDone = true;
+
     this.function = new Function(
       scope,
       `${id}-MigrationsChecker`,
-      async (
-        event: CloudFormationCustomResourceEvent
-      ): Promise<CloudFormationCustomResourceResponse> => {
+      async (event: CloudFormationCustomResourceEvent): Promise<ReturnType> => {
         console.log(event);
 
         const client = new SFNClient({});
 
-        try {
-          for (const migration of migrationIdStateMachinePairs) {
-            const migrationEntry = await $AWS.DynamoDB.GetItem({
-              Table: migrationsHistoryTable,
-              Key: marshall({ id: migration.migrationId }),
-            });
+        for (const migration of migrationIdStateMachinePairs) {
+          const migrationEntry = await $AWS.DynamoDB.GetItem({
+            Table: migrationsHistoryTable,
+            Key: marshall({ id: migration.migrationId }),
+          });
 
-            if (!migrationEntry.Item) {
-              throw new Error(
-                `Failed to find migration entry for migrationId: ${migration.migrationId}`
-              );
-            }
-
-            const command = new DescribeExecutionCommand({
-              executionArn: migrationEntry.Item?.executionArn.S,
-            });
-            const response = await client.send(command);
-
-            console.log({ migration, response });
-
-            await $AWS.DynamoDB.PutItem({
-              Table: migrationsHistoryTable,
-              Item: marshall({
-                id: migration.migrationId,
-                startedAt: response.startDate?.toISOString()!,
-                executionArn: response.executionArn!,
-                status: response.status as ExecutionStatus,
-                endedAt: response.stopDate?.toISOString(),
-              }),
-            });
+          if (!migrationEntry.Item) {
+            throw new Error(
+              `Failed to find migration entry for migrationId: ${migration.migrationId}`
+            );
           }
-        } catch (error) {
-          console.log({ error });
 
-          return {
-            Status: "FAILED",
-            Reason: (error as Error).message,
-            PhysicalResourceId: "DYNAMODB_MIGRATIONS_MANAGER",
-            LogicalResourceId: event.LogicalResourceId,
-            StackId: event.StackId,
-            RequestId: event.RequestId,
-          };
+          const command = new DescribeExecutionCommand({
+            executionArn: migrationEntry.Item?.executionArn.S,
+          });
+          const response = await client.send(command);
+
+          console.log({ migration, response });
+
+          if (response.status === "RUNNING") {
+            allDone = false;
+          }
+
+          await $AWS.DynamoDB.PutItem({
+            Table: migrationsHistoryTable,
+            Item: marshall({
+              id: migration.migrationId,
+              startedAt: response.startDate?.toISOString()!,
+              executionArn: response.executionArn!,
+              status: response.status as ExecutionStatus,
+              endedAt: response.stopDate?.toISOString(),
+            }),
+          });
         }
 
-        return {
-          Status: "SUCCESS",
-          PhysicalResourceId: "DYNAMODB_MIGRATIONS_MANAGER",
-          LogicalResourceId: event.LogicalResourceId,
-          StackId: event.StackId,
-          RequestId: event.RequestId,
-        };
+        return { IsComplete: allDone };
       }
     );
   }
